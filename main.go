5dad7b2da60da3b377c74ca5e8695241186e322f
Alex <AlexGrimes.se@gmail.com>
2022-11-06 12:28:13 -0500

    added handlers for like and jokes

--- main.go
+++ main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
+
+type Joke struct {
+	ID    int    `json:"id" binding:"required"`
+	Likes int    `json:"likes"`
+	Joke  string `json:"joke" binding:"required"`
+}
+
+var jokes = []Joke{
+	{1, 0, "Did you hear about the restaurant on the moon? Great food, no atmosphere."},
+	{2, 0, "What do you call a fake noodle? An Impasta."},
+	{3, 0, "How many apples grow on a tree? All of them."},
+	{4, 0, "Want to hear a joke about paper? Nevermind it's tearable."},
+	{5, 0, "I just watched a program about beavers. It was the best dam program I've ever seen."},
+	{6, 0, "Why did the coffee file a police report? It got mugged."},
+	{7, 0, "How does a penguin build it's house? Igloos it together."},
+}
 
 func main() {
 	router := gin.Default()
@@ -30,14 +47,19 @@ func main() {
 
 func JokeHandler(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Jokes handler not implemented yet",
-	})
+	c.JSON(http.StatusOK, jokes)
 }
 
 func LikeJoke(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
-	c.JSON(http.StatusOK, gin.H{
-		"message": "LikeJoke handler not implemented yet",
-	})
+	if jokeid, err := strconv.Atoi(c.Param("jokeID")); err == nil {
+		for i := 0; i < len(jokes); i++ {
+			if jokes[i].ID == jokeid {
+				jokes[i].Likes += 1
+			}
+		}
+		c.JSON(http.StatusOK, &jokes)
+	} else {
+		c.AbortWithStatus(http.StatusNotFound)
+	}
 }
